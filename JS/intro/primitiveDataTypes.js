ba8900800e7fd2da42fde9f5e68c46163e392b62
Tracy Kiprop <tracyrop84@gmail.com>
2026-06-17 15:45:00 +0300

    learnt primitive datatypes in js

---
+++ JS/intro/primitiveDataTypes.js
@@ -0,0 +1,36 @@
+let number1=23;
+console.log(number1);
+let number2=3.14;
+console.log(number2);
+let number3=-36;
+
+
+console.log(number3);
+//string data types rep words
+let str1="double quotes i am tracy";
+console.log(str1);
+let str2='single quotes i am tracy';
+console.log(str2);
+let str3=`back ticks i am tracy`;
+console.log(str3);
+let str4="23";//23 is a string
+let str5='tracy said "23 is a number"'
+
+//booleans-true/false
+let johnIsMale=true;
+console.log(johnIsMale);
+let johnIsMarried=false;
+console.log(johnIsMarried);
+
+
+//null-lack of value
+let nullVal1=null;
+console.log(nullVal1);
+
+/*undefined-absence of a value
+never set something to undefined*/
+let undefinedVal1=undefined//dont do this
+console.log(undefinedVal1);//undefined
+let undefinedVal2;//js engine sets to undefined
+console.log(undefinedVal2)//undefined
+
