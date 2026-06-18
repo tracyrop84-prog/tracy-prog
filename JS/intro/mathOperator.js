9badffd25b39816247744fa54c7aad0b869f57a5
Tracy Kiprop <tracyrop84@gmail.com>
2026-06-18 15:56:03 +0300

    lesson on math operators

---
+++ JS/intro/mathOperator.js
@@ -0,0 +1,24 @@
+let numb1=23;
+console.log(`numb1 is ${numb1} its type is ${typeof numb1}`)
+numb1=40;
+console.log(`numb1 is ${numb1} its type is ${typeof numb1}`)
+let numb2=60;
+console.log(`numb2 is ${numb2} its type is ${typeof numb2}`);
+let numb3=numb1+numb2+20;
+console.log(`numb3 is ${numb3} its type is ${typeof numb3}`);
+let numb4=numb1*numb2;
+console.log(`numb4 is ${numb4} its type is ${typeof numb4}`);
+let numb5=numb1**3;
+console.log(`numb5 is ${numb5} its type is ${typeof numb5}`);
+let numb6=Math.pow(10,6)
+console.log(`numb6 is ${numb6} its type is ${typeof numb6}`);
+
+let numb7=28
+let numb8=19
+let divNumb=numb7/numb8;
+console.log(`divNumb is ${divNumb} its type is ${typeof divNumb} `);
+
+let numb9=29;
+let numb10=10;
+let modNumb=numb9%numb10;
+console.log(`modNumb is ${modNumb} its type is ${typeof modNumb}`);
