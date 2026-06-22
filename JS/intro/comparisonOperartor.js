364e1c56ebf0b7cbc187e3db4bf79fda03e8a4b1
Tracy Kiprop <tracyrop84@gmail.com>
2026-06-22 10:07:42 +0300

    completed comparison operators

--- JS/intro/comparisonOperartor.js
+++ JS/intro/comparisonOperartor.js
@@ -18,3 +18,25 @@ console.log(`val1=${val1}!==val2=${val2} Ans ${strictInequality} its type is${ty
 let inEquality=val1!=val2;
 console.log(`val1=${val1}!==val2=${val2} Ans ${inEquality} its type is${typeof inEquality}`);
 
+let numb3=20;
+let numb4=10;
+
+let str1="A";
+let str2="b";
+
+
+console.log(`numb3=${numb3} and numb4 = ${numb4}`);
+console.log(`str1= ${str1} and str2 = ${str2}`)
+
+console.log(`numb3>numb4 ${numb3>numb4}`);
+console.log(`str2>str1 ${str2>str1}`);
+
+console.log(`numb3>=numb4 ${numb3>=numb4}`);
+console.log(`str2>=str1 ${str2>=str1 }`);
+
+console.log(`numb3<numb4 ${numb3<numb4}`);
+console.log(`str2<str1 ${str2<str1 }`);
+
+console.log(`numb3<=numb4 ${numb3<=numb4}`);
+console.log(`str2<=str1 ${str2<=str1 }`);
+
