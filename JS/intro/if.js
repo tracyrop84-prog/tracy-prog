51ee0bd9c8fda1602332ec3d7b9fe895df60f648
Tracy Kiprop <tracyrop84@gmail.com>
2026-06-24 10:55:48 +0300

    learnt on if conditions

---
+++ JS/intro/if.js
@@ -0,0 +1,18 @@
+let grade=prompt("enter your grade:")
+alert(`you entered${grade}`)
+
+if(grade>90){
+    alert("Excellent!")
+}
+if(grade>80){
+    alert("Very Good!")
+}
+if(grade=70){
+    alert("Good!")
+}
+if(grade>60){
+    alert("fair!")
+}
+if(grade>50){
+    alert("fail!")
+}
