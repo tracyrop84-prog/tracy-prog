f79cb98db2e2d3cc974855ac8fb80b981264e63e
Tracy Kiprop <tracyrop84@gmail.com>
2026-06-25 07:56:43 +0300

    assignement on working on if statements to get only one alert

--- JS/intro/if.js
+++ JS/intro/if.js
@@ -1,18 +1,18 @@
-let grade=prompt("enter your grade:")
-alert(`you entered${grade}`)
+let grade = prompt("enter your grade:");
+alert(`you entered${grade}`);
 
-if(grade>90){
-    alert("Excellent!")
+if (grade >= 90) {
+  alert("Excellent!");
 }
-if(grade>80){
-    alert("Very Good!")
+if (grade >= 80 && grade < 90) {
+  alert("Very Good!");
 }
-if(grade>70){
-    alert("Good!")
+if (grade >= 70 && grade < 80) {
+  alert("Good!");
 }
-if(grade>60){
-    alert("fair!")
+if (grade >= 60 && grade < 70) {
+  alert("fair!");
 }
-if(grade>50){
-    alert("fail!")
+if (grade >= 50 && grade < 60) {
+  alert("fail!");
 }
