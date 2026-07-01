3de6f481359b2b1123f06d560a262c513440da05
Tracy Kiprop <tracyrop84@gmail.com>
2026-07-01 12:11:39 +0300

    learnt on statements and expression

---
+++ JS/junior-dev/statementsAndExpressions.js
@@ -0,0 +1,9 @@
+//expression-piece of code that produces a value
+let n1 = 23;
+function sayMyName() {
+  console.log(`tracy rop`);
+}
+console.log(n1);
+console.log(10 + 12);
+console.log(n1 > 10);
+sayMyName();
