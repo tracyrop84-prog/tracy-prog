c3960abe8e6858430a15b8cf85841b9e20ed181f
Tracy Kiprop <tracyrop84@gmail.com>
2026-07-01 12:13:21 +0300

    learning types of functions

---
+++ JS/junior-dev/typesOfFunctions.js
@@ -0,0 +1,17 @@
+function areaCircle(r) {
+  return 3.142 * r * r;
+}
+
+let age = 23;
+let samAge = age;
+let ac = areaCircle;
+ac(10);
+console.log(samAge);
+
+console.log(`value of ac is ${ac} its typeof is ${typeof ac}`);
+
+let g = function () {
+  console.log(`hello`);
+};
+
+g();
