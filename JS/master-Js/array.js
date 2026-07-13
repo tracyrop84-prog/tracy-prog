6a06b9306f400a2b543f3c3ab1ea2ab2e65e67e2
Tracy Kiprop <tracyrop84@gmail.com>
2026-07-13 14:22:47 +0300

    covered arrays

---
+++ JS/master-Js/array.js
@@ -0,0 +1,38 @@
+const array1 = [
+  "Mazda",
+  "MX-5 Miata",
+  "220km/hr",
+  {
+    cylinder: "4",
+    number: "2.0 litres",
+  },
+  {
+    name: "Mazda Motor Corporation",
+    country: "Japan",
+    2024: "year manufactured",
+  },
+  function () {
+    alert(`car:${car.name}
+        model:${car.model}
+    topSpeed:${car.topSpeed}`);
+  },
+  `its known for it "jinba ittai" philosophy,which means"horse and rider as one"`,
+  true,
+  [5, 67, 6],
+];
+console.log(typeof array1);
+console.log(array1.length);
+console.log(array1[0]);
+console.log(array1[1]);
+console.log(array1[2]);
+console.log(array1[3]);
+console.log(array1[4]);
+console.log(array1[5]);
+console.log(array1[6]);
+console.log(array1[7]);
+console.log(array1[8][2]);
+
+// console.table(array1);
+/**
+ * array.methods-built in finctions
+ * * */
---
+++ JS/master-Js/arrayIsAnObject.js
@@ -0,0 +1,9 @@
+const ar = [3, 5];
+const arobj = {
+  0: 3,
+  1: 5,
+  length: 2,
+};
+console.table(ar);
+console.table(arobj);
+ar.push(11);
