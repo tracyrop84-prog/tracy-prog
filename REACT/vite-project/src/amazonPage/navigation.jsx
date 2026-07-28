e16da51e08d8ee1fb5c74024bd50962ed6fb2638
Tracy Kiprop <tracyrop84@gmail.com>
2026-07-28 15:18:52 +0300

    completed the navigation section of amazon page

---
+++ REACT/vite-project/src/amazonPage/navigation.jsx
@@ -0,0 +1,9 @@
+import logo from "./assets/amazonlogo.png"
+
+function Navigation(){
+    return <div>
+        <h1>Navigation Section</h1>
+        
+    </div>
+}
+export default Navigation
--- REACT/vite-project/src/main.jsx
+++ REACT/vite-project/src/main.jsx
@@ -1,54 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-function MyComponent() {
-  return null;
-}
-function MyComponent2() {
-  return (
-    <div>
-      <h1>hello world</h1>
-    </div>
-  );
-}
-function MyComponent3() {
-  return (
-    <>
-      <div>
-        <h3>react</h3>
-      </div>
-      <div>
-        <h2>javascript</h2>
-      </div>
-    </>
-  );
-}
-export function ParentComponent() {
-  return (
-    <div>
-      <h1>this is my parent component</h1>
-      <MyComponent />
-      <MyComponent></MyComponent>
+import Navigation from "./amazonPage/navigation";
 
-      <MyComponent2 />
-      <MyComponent2></MyComponent2>
-
-      <MyComponent3 />
-      <MyComponent3></MyComponent3>
-    </div>
-  );
-}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <h1>My First React application</h1>
-    <MyComponent />
-    <MyComponent></MyComponent>
-
-    <MyComponent2 />
-    <MyComponent2></MyComponent2>
-
-    <MyComponent3 />
-    <MyComponent3></MyComponent3>
-    <ParentComponent />
+    <Navigation/>
   </StrictMode>,
 );
